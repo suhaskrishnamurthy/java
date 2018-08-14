ded43be5f35f99487a2b3a56618c02ea24147b54
suhaskrishnamurthy <suhasvk1911@gmail.com>
2018-08-14 15:07:41 +0530

    converted from palindrome of numbers to palindrome of string

---
+++ palindrome/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String args[]) {
+		String a,b="";
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the string");
+		a=s.nextLine();
+		int n=a.length();
+		for(int i=n-1;i>=0;i--)
+		{
+		    b=b+a.charAt(i);
+		}
+		if(a.equalsIgnoreCase(b))
+		{
+		    System.out.println("palindrome");
+		}
+		else
+		System.out.println("not palindrome");
+	}
+}
+
