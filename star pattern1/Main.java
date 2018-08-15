1692986e724f6b326e7840cc2cd2c56c5bacc64c
suhaskrishnamurthy <suhasvk1911@gmail.com>
2018-08-16 00:31:11 +0530

    right angled triangle pattern

---
+++ star pattern1/Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    public static void stars(int n)
+    {
+        int i,j;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+        int n=5;
+        stars(n);
+    }
+}
